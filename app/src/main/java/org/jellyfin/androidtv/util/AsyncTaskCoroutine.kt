f763eae2ebe3cc5e82ea7a969cb0359678d0498b
TwistedUmbrellaX <twistedumbrella@gmail.com>
2024-10-04 20:54:19 -0400

    Convert remaining AsyncTask instances

---
+++ app/src/main/java/org/jellyfin/androidtv/util/AsyncTaskCoroutine.kt
@@ -0,0 +1,37 @@
+package org.jellyfin.androidtv.util
+
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+/*
+ * https://stackoverflow.com/a/63302963/461982
+ */
+abstract class AsyncTaskCoroutine<I> {
+	var job: Job? = null
+	open fun onPreExecute() {}
+
+	open fun onPostExecute() {}
+	abstract fun doInBackground(vararg params: I)
+
+	@OptIn(DelicateCoroutinesApi::class)
+	fun <T> execute(vararg input: I) {
+		job = GlobalScope.launch(Dispatchers.Main) {
+			onPreExecute()
+			callAsync(*input)
+		}
+	}
+
+	@OptIn(DelicateCoroutinesApi::class)
+	private suspend fun callAsync(vararg input: I) {
+		GlobalScope.async(Dispatchers.IO) {
+			 doInBackground(*input)
+		}.await()
+		GlobalScope.launch(Dispatchers.Main) {
+			onPostExecute()
+		}
+	}
+}
